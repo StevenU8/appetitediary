048c80f5a570dafca58a3458180e6a7eee7b3c88
Steven U <steven.h.u@outlook.com>
2020-12-13 17:07:19 -0800

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,54 +1,46 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
+
+import {
+  Navbar,
+  Nav
+} from "react-bootstrap";
+
+import About from "./Pages/About"
+import Home from "./Pages/Home"
+import Recipes from "./Pages/Recipes";
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="/">appetitediary</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/recipes">Recipes</Nav.Link>
+              <Nav.Link href="/about">About Us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/about">
             <About />
           </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/recipes">
+            <Recipes />
           </Route>
           <Route path="/">
             <Home />
           </Route>
         </Switch>
-      </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
